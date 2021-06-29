307af13280b46934d072ea9618d16901a04285e8
JSheleg <jessica.sheleg@gmail.com>
2021-06-29 15:19:11 -0500

    generate html file

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileDate = profileDataArr =>{
-    // This ...
-    for(let i = 0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
+// const printProfileDate = profileDataArr => {
+//     // This ...
+//     for(let i = 0; i < profileDataArr.length; i++){
+//         console.log(profileDataArr[i]);
+//     }
 
-    console.log('============');
+//     console.log('============');
 
-    // Is the same as this ...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+//     // Is the same as this ...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
     
-};
+// };
 
-printProfileDate(profileDataArgs);
+// printProfileDate(profileDataArgs);
+
+
+
+fs.writeFile("./index.html", generatePage(name,github),err =>{
+    if(err) throw new Error(err);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
